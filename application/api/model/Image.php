acb3cacd8cd6816adc8ed98d58693d8f905f352a
timber.chen <timberchen2008@gmail.com>
2018-09-29 15:32:12 +0800

    model attr getter

--- application/api/model/Image.php
+++ application/api/model/Image.php
@@ -9,4 +9,12 @@ class Image extends Model
     protected $table = 'image';
     protected $hidden = ['update_time', 'delete_time'];
     //
+
+    public function getUrlAttr($value, $data) {
+        $finalUrl = $value;
+        if ($data['from'] == 1) {
+            $finalUrl = config('setting.img_prefix').$value;
+        }
+        return $finalUrl;
+    }
 }
---
+++ application/extra/setting.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * File name:setting.php
+ * Author:timber.chen
+ * Version:1.0
+ * Description:
+ * History:
+ * Created on 9/29/18 3:15 PM by timber.chen
+ */
+
+return [
+    'img_prefix' => 'http://cha.cn/images',
+];
